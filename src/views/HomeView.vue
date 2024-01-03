9eb95f0daab6086db9ecfd413387de18cd7e9b0e
axelthereal <mail.axeltokam@gmail.com>
2024-01-04 00:28:32 +0100

    Added a customer review banner
    created an added customer reviews banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -40,6 +40,19 @@
 .banner-nav .banner-nav-item.active {
   opacity: 0.6;
 }
+
+.review-img {
+  width: 70px !important;
+  height: 70px !important;
+  border-radius: 50% !important;
+  object-fit: cover;
+}
+
+button.round-carousel-btn {
+  width: 13px;
+  height: 13px;
+  border-radius: 50% !important;
+}
 </style>
 
 <template>
@@ -230,6 +243,96 @@
           <ProductItemCard class="col-11 col-lg-3" />
           <ProductItemCard class="col-11 col-lg-3" />
           <ProductItemCard class="col-11 col-lg-3" />
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Customers-Testimonials -->
+    <div class="reviews-banner py-4 my-4">
+      <div class="container bg-color-secondary p-4 my-5 rounded rounded-1">
+        <div class="row row-cols-1 row-cols-lg-2 py-4 ps-lg-4 align-items-center">
+          <div class="banner-desc col-11 col-lg-5">
+            <h1 class="color-primary">What they say about us</h1>
+            <p class="fs-5 color-black opacity-75">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aliquam sit
+              veniam cumat, gotask atque.
+            </p>
+            <LinkButton btn-theme="primary-btn" btn-label="Leave a review" btn-url="" btn-icon="chat-right-text"
+              outlined="true" class="btn-lg mt-3" />
+          </div>
+
+          <div class="reviews col-11 col-lg-7">
+
+            <!-- CUSTOMERS REVIEWS... -->
+            <div id="myCarousel" class="carousel slide mb-6 pointer-event" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0"
+                  class="round-carousel-btn bg-color-primary" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"
+                  class="round-carousel-btn bg-color-primary"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"
+                  class="round-carousel-btn bg-color-primary active" aria-current="true"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item">
+
+                  <div class="container">
+                    <div class="review-item my-3 text-center w-75 mx-auto mb-5 pb-3">
+
+                      <img src="../assets/images/girl-customer-service-image.jpg" class="mx-auto  review-img" alt="" />
+                      <p class="opacity-80 my-3">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis saepe dolorem ducimus
+                        consectetur laudantium! Aperiam vero praesentium in odio sunt? Eum recusandae eligendi earum cum
+                        id perspiciatis eaque sint quas?
+                      </p>
+                      <h5>Client Name</h5>
+                      <span class="fw-semibold small opacity-50">Country</span>
+
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+
+                  <div class="container">
+                    <div class="review-item my-3 text-center w-75 mx-auto mb-5 pb-3">
+
+                      <img src="../assets/images/girl-customer-service-image.jpg" class="mx-auto  review-img" alt="" />
+                      <p class="opacity-80 my-3">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis saepe dolorem ducimus
+                        consectetur laudantium! Aperiam vero praesentium in odio sunt? Eum recusandae eligendi earum cum
+                        id perspiciatis eaque sint quas?
+                      </p>
+                      <h5>Client Name</h5>
+                      <span class="fw-semibold small opacity-50">Country</span>
+
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item active">
+
+                  <div class="container">
+                    <div class="review-item my-3 text-center w-75 mx-auto mb-5 pb-3">
+
+                      <img src="../assets/images/girl-customer-service-image.jpg" class="mx-auto  review-img" alt="" />
+                      <p class="opacity-80 my-3">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis saepe dolorem ducimus
+                        consectetur laudantium! Aperiam vero praesentium in odio sunt? Eum recusandae eligendi earum cum
+                        id perspiciatis eaque sint quas?
+                      </p>
+                      <h5>Client Name</h5>
+                      <span class="fw-semibold small opacity-50">Country</span>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- ...CUSTOMERS REVIEWS... -->
+
+
+
+          </div>
         </div>
       </div>
     </div>
