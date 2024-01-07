c6d3bb7b6b85ae2304455f05f0f86059fbebf435
axelthereal <mail.axeltokam@gmail.com>
2024-01-07 20:57:31 +0100

    modified app component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import HomeView from "./views/HomeView.vue"
+import HomeView from "./views/HomeView.vue";
 import { RouterView } from "vue-router";
 </script>
 
 <template>
+    <!-- App-File -->
     <div id="App">
         <RouterView />
     </div>
