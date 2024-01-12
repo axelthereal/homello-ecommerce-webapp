9d64cebc06ea988c96abcd36baef9f70086d099b
axelthereal <mail.axeltokam@gmail.com>
2024-01-12 21:00:48 +0100

    Edited about us view
    added basic component tags

--- src/views/AboutusView.vue
+++ src/views/AboutusView.vue
@@ -1,6 +1,23 @@
+<script setup>
+</script>
+
 <style scoped></style>
 
 <template></template>
 
 <script>
+export default {
+    name: "AboutusView",
+    data() {
+        return {}
+    },
+    methods: {
+        setPageTitle() {
+            document.title = "Homello | About Us";
+        },
+    },
+    beforeMount() {
+        this.setPageTitle()
+    }
+}
 </script>
