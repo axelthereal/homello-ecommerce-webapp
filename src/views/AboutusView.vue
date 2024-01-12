084e7e5de0990105eeaa2c6d30bd91065960292c
axelthereal <mail.axeltokam@gmail.com>
2024-01-13 00:28:59 +0100

    Redesign Content Banner on About us page

--- src/views/AboutusView.vue
+++ src/views/AboutusView.vue
@@ -3,7 +3,96 @@ import BasicNavbar from '../components/headerComponents/BasicNavbar.vue';
 import LinkButton from '../components/btnComponents/GlobalButtons/LinkButton.vue';
 </script>
 
-<style scoped></style>
+<style scoped> .img-wrap img {
+     border-radius: 20px;
+ }
+
+ .img-fluid {
+     max-width: 100%;
+     height: auto;
+ }
+
+ img,
+ svg {
+     vertical-align: middle;
+ }
+
+ #features .col-lg-6 {
+     position: relative !important;
+ }
+
+ #features .grid-sm {
+     position: absolute;
+     z-index: 1 !important;
+     -webkit-transform: translate(-30%, -55%);
+     -ms-transform: translate(-30%, -55%);
+     transform: translate(-30%, -55%);
+     overflow: hidden;
+     width: 50%;
+ }
+
+ #features .grid-sm img {
+     width: 100%;
+     max-width: 100%;
+     height: 100%;
+     object-fit: cover;
+ }
+
+ #features .grid-lg {
+     z-index: 1 !important;
+     overflow: hidden;
+     width: 100%;
+ }
+
+ #features .grid-lg img {
+     max-width: 100%;
+     width: 100%;
+     height: 100%;
+     object-fit: cover;
+ }
+
+ #features .icon {
+     position: relative;
+ }
+
+ #features .icon::after {
+     top: 0px;
+     width: 38px;
+     height: 38px;
+     content: "";
+     opacity: 0.4;
+     border-radius: 50% !important;
+     position: absolute;
+     background-color: #357979;
+     display: block;
+     -webkit-transform: translate(50%, 50%);
+     -ms-transform: translate(50%, 50%);
+     transform: translate(50%, 50%);
+     z-index: -1;
+ }
+
+ .img-wrap::before {
+     position: absolute;
+     content: "";
+     width: 255px;
+     height: 217px;
+     background-image: url("../assets/images/dots-light.svg");
+     background-repeat: no-repeat;
+     background-size: contain;
+     -webkit-transform: translate(25%, -25%);
+     -ms-transform: translate(25%, -25%);
+     transform: translate(25%, -25%);
+     z-index: -1;
+     right: 0px !important;
+ }
+
+
+ *,
+ ::after,
+ ::before {
+     box-sizing: border-box;
+ }
+</style>
 
 <template>
     <!-- Navbar -->
@@ -33,13 +122,13 @@ import LinkButton from '../components/btnComponents/GlobalButtons/LinkButton.vue
     <!-- ../Header-Banner -->
 
     <!-- Features-header -->
-    <section class="py-5 border-bottom" id="features">
+    <section class="py-5" id="features">
         <div class="container px-5 my-5">
             <div class="row justify-content-between align-items-center">
                 <!-- Container -->
 
                 <!-- text-bx -->
-                <div class="col-lg-6">
+                <div class="col-lg-5">
                     <div class="header">
                         <h1 class="h1 fw-bolder">Why Choose Us</h1>
                         <p class="fw-normal lead mb-3">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
@@ -51,36 +140,43 @@ import LinkButton from '../components/btnComponents/GlobalButtons/LinkButton.vue
                     </div>
                     <div class="row gx-5">
                         <div class="col-6 col-md-6 mb-5 mb-lg-0 my-4">
-                            <span><i class="bi bi-collection"></i>
+                            <span class="icon h1 fw-bolder mb-3 d-block"><i class="bi bi-truck"></i>
                             </span>
-                            <h2 class="h4 fw-bolder">Featured title</h2>
+                            <h2 class="h5 fw-bolder">Fast & Free Shipping</h2>
                             <p>Paragraph of text beneath the heading to explain the heading.</p>
                         </div>
                         <div class="col-6 col-md-6 mb-5 mb-lg-0 my-4">
-                            <span><i class="bi bi-building"></i>
+                            <span class="icon h1 fw-bolder mb-3 d-block"><i class="bi bi-bag-check"></i>
                             </span>
-                            <h2 class="h4 fw-bolder">Featured title</h2>
+                            <h2 class="h5 fw-bolder">Easy to Shop</h2>
                             <p>Paragraph of text beneath the heading to explain the heading.</p>
                         </div>
                         <div class="col-6 col-md-6 mb-5 mb-lg-0 my-4">
-                            <span><i class="bi bi-toggles2"></i>
+                            <span class="icon h1 fw-bolder mb-3 d-block"><i class="bi bi-headset"></i>
                             </span>
-                            <h2 class="h4 fw-bolder">Featured title</h2>
+                            <h2 class="h5 fw-bolder">24h/7 Support</h2>
                             <p>Paragraph of text beneath the heading to explain the heading.</p>
                         </div>
                         <div class="col-6 col-md-6 mb-5 mb-lg-0 my-4">
-                            <span><i class="bi bi-toggles2"></i>
+                            <span class="icon h1 fw-bolder mb-3 d-block"><i class="bi bi-arrow-repeat"></i>
                             </span>
-                            <h2 class="h4 fw-bolder">Featured title</h2>
+                            <h2 class="h5 fw-bolder">Hassle Free Returns</h2>
                             <p>Paragraph of text beneath the heading to explain the heading.</p>
                         </div>
                     </div>
                 </div>
 
                 <!-- img-bx -->
-                <div class="col-lg-6">
+                <div class="col-lg-6 mt-5 mt-lg-0">
                     <div class="img-wrap">
-                        <img src="images/why-choose-us-img.jpg" alt="Image" class="img-fluid">
+                        <div class="img-grid grid-lg">
+                            <img src="../assets/images/image01.jpg" alt="Image" class="img-fluid">
+                        </div>
+                        <div class="img-grid grid-sm">
+                            <img src="../assets/images/img-grid-6.jpg" alt="Image" class="img-fluid">
+                        </div>
+
+
                     </div>
                 </div>
 
