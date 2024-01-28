7ef6b4db9a89144d3942e9fc1c3a1ef9a1c06c6b
axelthereal <mail.axeltokam@gmail.com>
2024-01-28 17:19:02 +0100

    edited cart footer

--- src/views/ShoppingcartView.vue
+++ src/views/ShoppingcartView.vue
@@ -1,9 +1,10 @@
 <script setup>
 // import_required_components
 import GlobalNavbar from "../components/headerComponents/GlobalNavbar.vue";
+import LinkButton from "../components/btnComponents/GlobalButtons/LinkButton.vue";
 </script>
 
-<style>
+<style scoped>
 .navbar,
 .mainbanner {
     background-color: #f1f1f1 !important;
@@ -71,24 +72,68 @@ import GlobalNavbar from "../components/headerComponents/GlobalNavbar.vue";
             <!-- Body-cotent-footer -->
             <div class="content-footer mt-5 pt-3">
 
-                <!-- Cart-Info -->
-                <div class="cart-total">
-                    <h2 class="fw-bold display-6 opacity-80 mb-3 border-bottom">Cart Total</h2>
-                    <div class="py-1 d-flex gap-2 w-100 justify-content-between">
-                        <span class="label me-5 fw-semibold">
-                            No. Products
-                        </span>
-                        <span class="label fw-bold text-nowrap">8</span>
-                    </div>
-                    <div class="py-1 d-flex gap-2 w-100 justify-content-between">
-                        <span class="label me-5 fw-semibold">
-                            Total Price
-                        </span>
-                        <span class="label fw-bold text-nowrap">175.25$</span>
-                    </div>
+                <div class="row">
+                    <!-- CART-FOOTER -->
 
+                    <!-- Cart-btns -->
+                    <div class="cart-btn">
+                        <div
+                            class="btns-bx d-flex justify-content-center gap-2 align-items-center w-100 cart-bx-content my-3 my-lg-1">
+                            <button
+                                class="rounded rounded-0 py-2 fw-semibold my-3
+                        btn btn-light border border-1 bg-body-tertiary d-flex justify-content-center gap-2 align-items-center w-100"
+                                type="button">
+                                <span class="me-2">Clear Cart</span>
+                                <span class="bi bi-trash3"></span>
+                            </button>
+                            <LinkButton btn-theme="dark-btn" btn-label="Continue Shopping" btn-url="" btn-icon="cart-check"
+                                outlined="false" class="py-2 btn-lg my-3 w-100" />
+                        </div>
+                        <div class="coupon cart-bx-content my-3 my-lg-1">
+                            <div class="header">
+                                <h3 class="fw-bold   opacity-80 ">Coupon</h3>
+                                <span class="lead fw-normal opacity-75">Enter your coupon code if you have one.</span>
+                            </div>
+                            <div class="form d-flex justify-content-center gap-2 align-items-center w-100">
+                                <input type="text"
+                                    class="rounded rounded-0 py-2 fw-semibold my-3
+                        form-control border border-1 border-tertiary  d-flex justify-content-center gap-2 align-items-center w-100"
+                                    placeholder="Coupon Code">
+                                <button class="rounded rounded-0 py-2 fw-semibold my-3
+                        btn btn-dark border border-1  d-flex justify-content-center gap-2 align-items-center w-100"
+                                    type="button">
+                                    Apply Coupon
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- ...Cart-btns -->
+
+
+                    <!-- Cart-Info -->
+                    <div class="cart-total">
+                        <h2 class="fw-bold display-6 opacity-80 mb-3 border-bottom">Cart Total</h2>
+                        <div class="py-1 d-flex gap-2 w-100 justify-content-between">
+                            <span class="label me-5 fw-semibold">
+                                No. Products
+                            </span>
+                            <span class="label fw-bold text-nowrap">8</span>
+                        </div>
+                        <div class="py-1 d-flex gap-2 w-100 justify-content-between">
+                            <span class="label me-5 fw-semibold">
+                                Total Price
+                            </span>
+                            <span class="label fw-bold text-nowrap">175.25$</span>
+                        </div>
+
+                        <LinkButton btn-theme="dark-btn" btn-label="Proceed To Checkout" btn-url="" btn-icon="arrow-right"
+                            outlined="false" class="btn-lg mt-3 w-100" />
+
+                    </div>
+                    <!-- cart-info -->
+
+                    <!-- ...CART-FOOTER -->
                 </div>
-                <!-- -->
 
 
             </div>
