60974d05716005a5be37fd1542c8b480514b5345
axelthereal <mail.axeltokam@gmail.com>
2024-01-28 11:24:31 +0100

    Edit ShoppingCart view

--- src/views/ShoppingcartView.vue
+++ src/views/ShoppingcartView.vue
@@ -1,7 +1,14 @@
 <script setup>
+// import_required_components
+import GlobalNavbar from "../components/headerComponents/GlobalNavbar.vue";
 </script>
 
-<style></style>
+<style>
+.navbar,
+.mainbanner {
+    background-color: #f1f1f1 !important;
+}
+</style>
 
 <template></template>
 
