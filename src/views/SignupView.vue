c8a9c86f652071d68d8d2291c8fabb5e26e357a3
axelthereal <mail.axeltokam@gmail.com>
2024-01-30 22:20:02 +0100

    made changes to form display in Signup view

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -16,18 +16,22 @@ nav,
     <!-- ..Header -->
 
     <!-- MainBanner -->
-    <main class="form-signin w-100 m-auto">
-        <form class="form-bx col-lg-4 col-md-10 col-12 mx-auto p-lg-5">
-            <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <main class="form-signin w-100 m-auto pt-5">
+        <form class="form-bx col-lg-4 col-10 mx-auto p-lg-5 container mt-5">
 
-            <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
+            <h1 class="display-6 mb-1 fw-bold d-flex align-items-center justify-content-center">Create a new account</h1>
+            <p class="w-80 mx-auto d-flex align-items-center justify-content-center opacity-50" align="center">Sign up to
+                start shopping securely
+                and get access to the best quality home & office products !</p>
+
+            <div class="form-floating my-3">
+                <input type="email" class="form-control rounded rounded-0" id="floatingEmail"
+                    placeholder="name@example.com">
+                <label for="floatingEmail" class="text-muted">Email address</label>
             </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
+            <div class="form-floating my-3">
+                <input type="password" class="form-control rounded rounded-0" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword" class="text-muted">Password</label>
             </div>
 
             <div class="form-check text-start my-3">
@@ -36,7 +40,10 @@ nav,
                     Remember me
                 </label>
             </div>
-            <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+            <button
+                class="btn btn-light bg-color-primary w-100 py-2 text-white fs-5 d-flex align-items-center justify-content-center rounded-0"
+                type="submit">Sign
+                in</button>
             <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
         </form>
     </main>
