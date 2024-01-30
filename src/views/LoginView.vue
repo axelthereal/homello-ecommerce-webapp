4a9852aebefd66aa7c460b04646c833ed66a6820
axelthereal <mail.axeltokam@gmail.com>
2024-01-30 22:55:45 +0100

    edit LoginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,10 +3,14 @@
 <template></template>
 
 <script>
+import GlobalNavbar from "../components/headerComponents/globalnavbar.vue";
+import LinkButton from "../components/btnComponents/GlobalButtons/LinkButton.vue";
+
 export default {
     name: "LoginView",
     components: {
-
+        GlobalNavbar,
+        LinkButton
     }
 }
 </script>
