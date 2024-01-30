aa61412ae3a094853e5359087bea0c90efa88133
axelthereal <mail.axeltokam@gmail.com>
2024-01-30 22:58:50 +0100

    edited Login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -35,6 +35,50 @@ button:active {
     <!-- Header -->
     <GlobalNavbar />
     <!-- ..Header -->
+
+    <!-- MainBanner -->
+    <main class="form-signin w-100 m-auto pt-5">
+        <div class="formbx col-lg-4 col-10 mx-auto p-lg-5 container mt-lg-0 mt-4">
+            <form class="w-100 form">
+
+                <h1 class="display-6 mb-1 fw-bold d-flex align-items-center justify-content-center">Welcome back !
+                </h1>
+                <p class="w-80 mb-4 mx-auto d-flex align-items-center justify-content-center opacity-50" align="center">Sign
+                    in to your account and start shopping </p>
+
+                <div class="form-floating my-3">
+                    <input type="email" class="form-control bg-light rounded rounded-0" id="floatingEmail"
+                        placeholder="name@example.com">
+                    <label for="floatingEmail" class="text-muted">Email address</label>
+                </div>
+                <div class="form-floating my-3">
+                    <input type="password" class="form-control bg-light rounded rounded-0" id="floatingPassword"
+                        placeholder="Password">
+                    <label for="floatingPassword" class="text-muted">Password</label>
+                </div>
+
+                <div class="form-check text-start my-3">
+                    <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Remember me
+                    </label>
+                </div>
+                <button
+                    class="btn btn-light primary-btn w-100 py-2 text-white d-flex align-items-center justify-content-center rounded-0"
+                    type="submit">Login</button>
+            </form>
+
+            <div class="formbtns mt-3 pt-3 d-flex justify-content-evenly align-items-center gap-4">
+                <span class="w-100 d-flex line my-1 rounded rounded-3"></span>
+                <span class="lead fw-semibold">OR</span>
+                <span class="w-100 d-flex line my-1 rounded rounded-3"></span>
+            </div>
+
+            <LinkButton btn-theme="dark-btn" btn-label="Create an account" btn-url="/login" btn-icon="person-plus"
+                outlined="false" class="py-2 btn-lg my-3 w-100" />
+        </div>
+    </main>
+    <!-- ../MainBanner -->
 </template>
 
 <script>
