d0fa5eda04b3876e9419b236c0a9bb47d4e49a7c
axelthereal <mail.axeltokam@gmail.com>
2024-01-30 22:56:55 +0100

    add Global navbar to login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,41 @@
-<style scoped></style>
+<style scoped>
+nav,
+.mainbanner {
+    background-color: #ffffff !important;
+}
 
-<template></template>
+input.form-control:focus,
+input.form-check-input:focus {
+    background-color: #ffffff !important;
+    border: 1px solid #0b9768 !important;
+}
+
+input.form-check-input:checked {
+    background-color: #0b9768 !important;
+    border: 1px solid #0b9768 !important;
+    box-shadow: 2px 3px 4px rgba(0, 0, 0, 0) !important;
+}
+
+input.form-check-input {
+    cursor: pointer;
+    box-shadow: none !important;
+}
+
+button:active {
+    background-color: #214e4e !important;
+}
+
+.line {
+    height: 2px;
+    background-color: #e8ecf0;
+}
+</style>
+
+<template>
+    <!-- Header -->
+    <GlobalNavbar />
+    <!-- ..Header -->
+</template>
 
 <script>
 import GlobalNavbar from "../components/headerComponents/globalnavbar.vue";
