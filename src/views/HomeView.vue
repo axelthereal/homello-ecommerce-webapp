edd3354bf6ce24b476ba05719a1794796293a238
axelthereal <mail.axeltokam@gmail.com>
2024-02-04 19:34:46 +0100

    minor changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -195,13 +195,13 @@
                   <div class="products-list row row-cols-1 row-cols-lg-3 d-flex justify-content align-items-stretch">
 
                     <ProductItemCard product-title="Ergonomic Chair" product-category="Homello" product-price="45.87"
-                      product-discount="" product-image="@/../src/assets/images/product-3.png"
+                      product-discount="" product-image="../../src/assets/images/product-3.png"
                       class="col-11 col-lg-4 d-block" />
                     <ProductItemCard product-title="Nordic Chair" product-category="Homello" product-price="67.00"
-                      product-discount="" product-image="@/../src/assets/images/product-1.png"
+                      product-discount="" product-image="../../src/assets/images/product-1.png"
                       class="col-11 col-lg-4 d-block" />
                     <ProductItemCard product-title="Kruzo Aero Chair" product-category="Homello" product-price="73.23"
-                      product-discount="" product-image="@/../src/assets/images/product-2.png"
+                      product-discount="" product-image="../../src/assets/images/product-2.png"
                       class="col-11 col-lg-4 d-block" />
                   </div>
                 </div>
